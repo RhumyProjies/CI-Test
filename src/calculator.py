0bce7bb89b443c1119420b7caf35b4c673567ad1
Rume7 <electengrhume@gmail.com>
2025-07-05 10:56:06 +0100

    Created a calculator with python

---
+++ src/calculator.py
@@ -0,0 +1,11 @@
+"""
+A simple calculator class for demonstrating CI/CD with GitHub Actions
+"""
+
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
